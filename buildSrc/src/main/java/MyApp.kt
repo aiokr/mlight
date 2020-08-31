b5d59eb56c3bf9d62d4076a4715685a91e8aa1e0
aiokr <aiokr@outlook.com>
2020-08-31 19:40:47 +0800

    add new icons

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "app.itypen.mlight"
-    const val version = 200831
-    const val versionName = "1.0.0"
+    const val version = 200901
+    const val versionName = "1.0.2"
 }
